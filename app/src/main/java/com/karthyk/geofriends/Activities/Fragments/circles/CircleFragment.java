0690e92b95522119d2e3af7b974e184086ea70e5
karthik-logs <karthik.periasami@mtap.in>
2016-01-19 16:43:24 +0530

    resize circle image in circles fragments

--- app/src/main/java/com/karthyk/geofriends/Activities/Fragments/circles/CircleFragment.java
+++ app/src/main/java/com/karthyk/geofriends/Activities/Fragments/circles/CircleFragment.java
@@ -24,6 +24,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class CircleFragment extends Fragment {
 
   ListView mListViewCircles;
+
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
@@ -42,8 +43,15 @@ public class CircleFragment extends Fragment {
 
   private class CircleListAdapter extends ArrayAdapter<UserList> {
 
+    ArrayList<UserList> userLists;
+
     public CircleListAdapter(Context context, ArrayList<UserList> users) {
       super(context, 0, users);
+      userLists = users;
+    }
+
+    @Override public UserList getItem(int position) {
+      return userLists.get(position);
     }
 
     @Override public View getView(int position, View convertView, ViewGroup parent) {
@@ -62,18 +70,20 @@ public class CircleFragment extends Fragment {
       viewHolder.mTextViewUserName.setText(user.name);
       Picasso.with(getActivity())
           .load(user.imageUrl)
-          .resize(70, 70)
+          .resize(50, 50)
           .into(new Target() {
             @Override public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
               viewHolder.mImageViewUserDP.setImageBitmap(bitmap);
             }
 
             @Override public void onBitmapFailed(Drawable errorDrawable) {
-
+              viewHolder.mImageViewUserDP.setImageDrawable(getResources().getDrawable(R.drawable
+                  .ic_default_user));
             }
 
             @Override public void onPrepareLoad(Drawable placeHolderDrawable) {
-
+              viewHolder.mImageViewUserDP.setImageDrawable(getResources().getDrawable(R.drawable
+                  .ic_default_user));
             }
           });
       return v;
@@ -83,6 +93,7 @@ public class CircleFragment extends Fragment {
   private class CircleListViewHolder {
     TextView mTextViewUserName;
     CircleImageView mImageViewUserDP;
+
     public CircleListViewHolder(View v) {
       mTextViewUserName = (TextView) v.findViewById(R.id.user_name);
       mImageViewUserDP = (CircleImageView) v.findViewById(R.id.user_dp);
